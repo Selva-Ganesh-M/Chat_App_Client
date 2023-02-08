33dd85e33218246e5a4ae8d8171f412d22e6460b
Selva Ganesh M <selvadev2k@gmail.com>
2023-02-08 13:59:18 +0530

    attachments section UI

--- src/components/Chat/ChatInput.jsx
+++ src/components/Chat/ChatInput.jsx
@@ -1,6 +1,10 @@
-import { IconButton, InputAdornment, Stack, TextField } from "@mui/material"
+import { Fab, IconButton, InputAdornment, Stack, TextField, Tooltip } from "@mui/material"
 import { styled } from "@mui/material/styles"
 import { LinkSimple, Smiley } from "phosphor-react"
+import { Actions } from "../../data"
+import { useTheme } from "@mui/material/styles"
+import React from "react"
+
 
 // styled inputBase
 const StyledInput = styled(TextField)(({ theme }) => ({
@@ -11,6 +15,9 @@ const StyledInput = styled(TextField)(({ theme }) => ({
 }))
 
 const InputBase = ({ setIsEmojiPickerOpen }) => {
+    const [isLinksOpen, setIsLinksOpen] = React.useState(false)
+
+    const theme = useTheme()
     return (
         < StyledInput
             placeholder={'send a message...'}
@@ -22,8 +29,35 @@ const InputBase = ({ setIsEmojiPickerOpen }) => {
                 // Adornment: are things that you can add to the start and end of an input InputBase
                 startAdornment: (
                     <Stack>
+                        {/* floater */}
+                        {
+                            isLinksOpen && (
+                                <Stack sx={{ position: "relative" }}>
+                                    <Stack sx={{ position: "absolute", bottom: 40, right: "-20px", backgroundColor: theme.palette.background.default }} spacing={3} p={1} borderRadius={5}>
+                                        {
+                                            Actions.map(item => (
+                                                <Tooltip title={item.title} placement="right">
+                                                    <Fab key={item.title} >
+                                                        {item.icon}
+                                                    </Fab>
+                                                </Tooltip>
+
+                                            ))
+                                        }
+
+                                    </Stack>
+                                    {/* {Actions.map(item => (
+                            <Fab sx={{ position: "absolute", top: -item.y }}>
+                                {item.icon}
+                            </Fab>
+                            ))} */}
+                                </Stack>
+                            )
+                        }
+
+                        {/* the link emoji */}
                         <InputAdornment >
-                            <IconButton>
+                            <IconButton onClick={() => setIsLinksOpen(prev => !prev)} onBlur={() => setIsLinksOpen(prev => !prev)} >
                                 <LinkSimple />
                             </IconButton>
                         </InputAdornment>
@@ -32,7 +66,7 @@ const InputBase = ({ setIsEmojiPickerOpen }) => {
                 endAdornment: (
                     <Stack>
                         <InputAdornment>
-                            <IconButton onClick={() => setIsEmojiPickerOpen(prev => !prev)}>
+                            <IconButton onClick={() => setIsEmojiPickerOpen(prev => !prev)} onBlur={() => setIsEmojiPickerOpen(prev => !prev)}>
                                 <Smiley />
                             </IconButton>
                         </InputAdornment>
--- src/data/index.js
+++ src/data/index.js
@@ -1,10 +1,14 @@
 import { faker } from "@faker-js/faker";
 import {
+  Camera,
   ChatCircleDots,
+  File,
   Gear,
   GearSix,
+  Image,
   Phone,
   SignOut,
+  Sticker,
   User,
   Users,
 } from "phosphor-react";
@@ -225,6 +229,40 @@ const Message_options = [
   },
 ];
 
+const Actions = [
+  {
+    color: "#4da5fe",
+    icon: <Image size={24} />,
+    y: 102,
+    title: "Photo/Video",
+  },
+  {
+    color: "#1b8cfe",
+    icon: <Sticker size={24} />,
+    y: 172,
+    title: "Stickers",
+  },
+  {
+    color: "#0172e4",
+    icon: <Camera size={24} />,
+    y: 242,
+    title: "Image",
+  },
+  {
+    color: "#0159b2",
+    icon: <File size={24} />,
+    y: 312,
+    title: "Document",
+  },
+  {
+    color: "#013f7f",
+    icon: <User size={24} />,
+    y: 382,
+    title: "Contact",
+  },
+];
+
+
 export {
   Profile_Menu,
   Nav_Setting,
@@ -232,4 +270,5 @@ export {
   ChatList,
   Chat_History,
   Message_options,
+  Actions
 };
