4847ac00ce4267abf78ba142d044d2180f726583
Selva Ganesh M <selvadev2k@gmail.com>
2023-02-10 22:08:52 +0530

    created register empty file

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,10 +1,11 @@
-import { Stack } from "phosphor-react";
+import { Typography } from "@mui/material";
+import { Stack } from "@mui/material";
 import React from "react";
 
 const Register = () => {
   return (
     <Stack spacing={2} alignItems={"center"}>
-      <div>hello</div>
+      <Typography>Hello</Typography>
     </Stack>
   );
 };
